7c5e7ff566916309232470c8c7497abb202477ed
Garen <karmartir@gmail.com>
2023-06-23 20:48:52 -0400

    plex type fully functional api

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
 
 
             {movies?.length > 0 ? (<div className="container">
-                    {movies.map((movie) => <MovieCard movie={movie}/>)}
+                    {movies.map((movie) => <MovieCard key={movie.title} movie={movie}/>)}
                 </div>) : (<div className="empty">
                     <h2> No movies found</h2>
                 </div>)}
