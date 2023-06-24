6d98639acd810336d0463d4de9198cfc1a44d784
Garen <karmartir@gmail.com>
2023-06-23 20:05:48 -0400

    plex type fully functional api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import {useEffect, useState} from "react";
+import MovieCard from "./MovieCard";
+
+const API_URL = " http://www.omdbapi.com/?i=tt3896198&apikey=e0b9314";
 
 const App = () => {
-  return (
-    <div className="App">
-    <h1>Hello, React!</h1>
-       
-    </div>
-  );
-}
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
+    const searchMovies = async (title) => {
+        const responce = await fetch(`${API_URL}&s=${title}`);
+        const data = await responce.json();
+        setMovies(data.Search);
+    };
+
+    useEffect(() => {
+        searchMovies("Shrek");
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(event) => setSearchTerm(event.target.value)}
+                />
+                <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)}/>
+            </div>
+
+
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                            {movies.map((movie) => <MovieCard movie={movie}/>)}
+                        </div>
+                    ) : (
+                        <div className="empty">
+                            <h2> No movies found</h2>
+                        </div>
+                    )
+            }
+
+
+        </div>
+    );
+};
 
 export default App;
